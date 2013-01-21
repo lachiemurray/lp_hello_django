eba451087d29e7daea9c4addeba80c3ec8a3fb6b
Lachlan Murray <lachie.murray@gmail.com>
2013-01-21 21:07:14 +0000

    Removed 'configure' entry point, not needed

--- urls.py
+++ urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
     url(r'^edition/$', 'lp_hello_django.views.edition'),
     url(r'^sample/$', 'lp_hello_django.views.sample'),
     url(r'^validate_config/$', 'lp_hello_django.views.validate_config'),
-    url(r'^configure/$', 'lp_hello_django.views.configure'),
     url(r'^meta.json$', 'lp_hello_django.views.meta_json'),
     url(r'^icon.png$', 'lp_hello_django.views.icon'),
     
